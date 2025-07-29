bc5b006e3d3882387d9c99348ab70c6c42cb862f
nkeita1 <neneawakobelekeita0@gmail.com>
2025-07-29 15:03:58 +0000

    adding app.py file

---
+++ app.py
@@ -0,0 +1,8 @@
+
+from flask import Flask 
+app = Flask(__name__)
+@app.route('/')
+def helloworld():
+    return 'Hello, Annuaire!'
+if __name__ == '__main__':
+    app.run(debug=True)  # debug=True permet de voir les erreurs et recharger auto
